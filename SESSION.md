b8f58fd99640f6c052be29b5a27255c4b13fda88
hoang <lebahoang010296@gmail.com>
2026-04-28 22:34:52 -0700

    day 2 building graph

--- SESSION.md
+++ SESSION.md
@@ -1,13 +1,25 @@
 ## Session 1 - day 1 - 2026/04/27
 
-## What I built
+### What I built
 - Proto-Value struct with data and label file
 - A print helper function
 - If passed by value: borrow, if passed by reference: no borrow
 
-
-## What confused me
+### What confused me
 - Zed debugger
 
-## What's next
+### What's next
 - Day 2: add children: Vec<Value> to Value, try to build a graph 
+
+
+## Session 2 - day 2 - 2026/04/28
+### What I built/learnt
+- Borrow: construct a graph that moves a variable then re-access it
+- Make an enum to show which operation produces this variable
+- Write a recursive function to print out the type of function
+
+### What confused me
+- clone() -> we need to implement it
+
+### What's next
+- Day 2 exercism.
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,74 @@
+#[derive(Debug)]
 struct Value {
     data: f64,
     label: String,
+    children: Vec<Value>,
+    op: Op,
+}
+
+#[derive(Debug)]
+enum Op {
+    Add,
+    Mul,
+    Tanh,
+    Leaf,
 }
 
 fn print_value(v: &Value) {
     println!("{}: {}", v.label, v.data);
 }
 
+fn describe(v: &Value) {
+    match v.op {
+        Op::Leaf => println!("{} is a leaf input", v.label),
+        Op::Add => println!("{} was produced by addition", v.label),
+        Op::Mul => println!("{} was produced by multiplication", v.label),
+        Op::Tanh => println!("{} was produced by tanh", v.label),
+    }
+
+    for child in &v.children {
+        describe(child);
+    }
+}
+
 fn main() {
-    let v = Value {
-        data: 3.0,
-        label: String::from("x"),
+    let a = Value {
+        data: 2.0,
+        label: "a".into(),
+        children: vec![],
+        op: Op::Leaf,
     };
-    print_value(&v);
+    let b = Value {
+        data: 3.0,
+        label: "b".into(),
+        children: vec![],
+        op: Op::Leaf,
+    };
+    let c = Value {
+        data: 5.0,
+        label: "c".into(),
+        children: vec![a, b],
+        op: Op::Add,
+    };
+
+    describe(&c);
+
+    // println!("{:?}", c);
     // print_value(&v);
+    //
+    // let x = Value {
+    // data: 2.0,
+    // label: "x".into(),
+    // children: vec![],
+    // };
+    // let d = Value {
+    //     data: 4.0,
+    //     label: "d".into(),
+    //     children: vec![x.clone()],
+    // };
+    // let e = Value {
+    //     data: 3.0,
+    //     label: "e".into(),
+    //     children: vec![x],
+    // }; // x already moved!
 }
